24f08f5e162de04999d312c1d621d9b0a6c7bfde
deanturpin <git@lvbtn.uk>
2017-07-05 08:15:30 +0100

    decode URI

--- wikiwatch.user.js
+++ wikiwatch.user.js
@@ -4,6 +4,7 @@
 // @description	Wiki helper
 // @namespace   deanturpin
 // @include		https://en.wikipedia.org/wiki/*
+// @include		https://en.wiktionary.org/wiki/*
 // @version     1
 // @grant       none
 // ==/UserScript==
@@ -16,8 +17,8 @@ const text = document.createTextNode("");
 var props = "";
 props += "text-align: left; font-family: serif;";
 props += "background-color: magenta; color: white;";
-props += "position: fixed; bottom: 0; left: 50%;";
-props += "width: 200px; height: 100px;";
+props += "position: fixed; top: 0; left: 0;";
+props += "width: 160px; height: 160px;";
 props += "opacity: 0.7;";
 props += "box-shadow: 10px 10px 5px #888;";
 
@@ -28,4 +29,4 @@ document.body.appendChild(popup);
 
 // Get page title
 const title = window.location.href.split("/").pop().replace(/_/g,' ');
-popup.innerHTML += "<i>" + title + "</i>";
+popup.innerHTML += "<i>" + decodeURI(title) + "</i>";
